50cbef5cce9bb2b2681bce53564999f52b6c24b6
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-10-27 21:22:31 +0600

    start

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config()
+
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/users', usersRouter);
+
+module.exports = app;
