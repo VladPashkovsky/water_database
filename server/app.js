e656f2a2ee79993ed789c07d27d4a472c7f5885a
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-10-27 22:35:10 +0600

    v.1.0. server_routes

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config()
 
+const PORT = process.env.PORT
+
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -12,6 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
---
+++ server/controllers/user-controller.js
@@ -0,0 +1,29 @@
+class UserController {
+
+  async register(req, res, next) {
+    try {
+      return res.json({message: 'Register response'})
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+      return res.json({message: 'Login response'})
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  async current(req, res, next) {
+    try {
+      return res.json({message: 'Current'})
+    } catch (e) {
+      next(e)
+    }
+  }
+
+}
+
+module.exports = new UserController()
