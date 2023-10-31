df9d9013ffd99d162dc3a3b914ef5d0961fae9e0
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-10-31 20:02:20 +0600

    v.3.4. user-controller_login

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -16,44 +16,10 @@ class UserController {
 
       const { name, email, password } = req.body
 
-      const userData = await userService.registration(name, email, password)
+      const userData = await userService.signUp(name, email, password)
       res.cookie('refreshToken', userData.refreshToken,
         { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true })
       return res.json(userData)
-
-      // const registeredUserByName = await prisma.user.findFirst({
-      //   where: { name },
-      // })
-      // const registeredUserByEmail = await prisma.user.findFirst({
-      //   where: { email },
-      // })
-      //
-      // if (registeredUserByName) {
-      //   return next(ApiError.RegisterWrongName())
-      // }
-      // if (registeredUserByEmail) {
-      //   return next(ApiError.RegisterWrongEmail())
-      // }
-
-      // const salt = await bcrypt.genSalt(10)
-      // const hashedPassword = await bcrypt.hash(password, salt)
-      //
-      // const user = await prisma.user.create({
-      //   data: { name, email, password: hashedPassword },
-      // })
-      //
-      // const secret = process.env.JWT_SECRET
-      //
-      // if (user && secret) {
-      //   res.status(201).json({
-      //     id: user.id,
-      //     email: user.email,
-      //     name,
-      //     token: jwt.sign({ id: user.id }, secret, { expiresIn: '30d' }),
-      //   })
-      // } else {
-      //   return res(ApiError.BadRequest('Bad Request...', errors.array()))
-      // }
     } catch (e) {
       next(e)
     }
@@ -63,31 +29,10 @@ class UserController {
     try {
       const { email, password } = req.body
 
-      if (!email || !password) {
-        return res.status(400).json({ message: 'Input required fields, please' })
-      }
-
-      const user = await prisma.user.findFirst({
-        where: { email },
-      })
-
-      const isPasswordCorrect = user && (await bcrypt.compare(password, user.password))
-      const secret = process.env.JWT_SECRET
-
-      if (user && isPasswordCorrect && secret) {
-        return res.status(200).json({
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          token: jwt.sign({ id: user.id }, secret, { expiresIn: '30d' }),
-        })
-      } else if (!user) {
-        return res.status(500).json({ message: 'Wrong email' })
-      } else if (!isPasswordCorrect) {
-        return res.status(500).json({ message: 'Wrong password' })
-      } else {
-        return res.status(500).json({ message: 'Wrong login or password' })
-      }
+      const userData = await userService.signIn(email, password)
+      res.cookie('refreshToken', userData.refreshToken,
+        { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true })
+      return res.json(userData)
     } catch (e) {
       next(e)
     }
--- server/services/user-service.js
+++ server/services/user-service.js
@@ -7,12 +7,7 @@ const UserDto = require('../dto/user-dto')
 
 class UserService {
 
-  async registration(name, email, password) {
-
-    // const candidate = await UserModel.findOne({ email })
-    // if (candidate) {
-    //   throw ApiError.BadRequest(`User with email ${email} is already exist`)
-    // }
+  async signUp(name, email, password) {
 
     const registeredUserByName = await prisma.user.findFirst({
       where: { name },
@@ -33,7 +28,7 @@ class UserService {
     const user = await prisma.user.create({
       data: { name, email, password: hashedPassword },
     })
-    
+
     const userDto = new UserDto(user)
     const tokens = tokenService.generateTokens({ ...userDto })
     await tokenService.saveToken(userDto.id, tokens.refreshToken)
@@ -41,6 +36,29 @@ class UserService {
     return { ...tokens, user: userDto }
   }
 
+  //==============================================================================================
+
+  async signIn(email, password) {
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    })
+
+    if (!user) {
+      throw ApiError.LoginWrongEmail()
+    }
+
+    const isPassEquals = await bcrypt.compare(password, user.password)
+    if (!isPassEquals) {
+      throw ApiError.LoginWrongPassword()
+    }
+
+    const userDto = new UserDto(user)
+    const tokens = tokenService.generateTokens({ ...userDto })
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+    return { ...tokens, user: userDto }
+  }
 }
 
 module.exports = new UserService()
