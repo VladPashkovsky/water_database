dcd7cc91de21e0b84e256437a9b3d7e367924934
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-11-04 19:12:43 +0600

    v.6.0. client_installed

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }))
 
 app.use('/api/users', usersRouter)
 app.use('/api/waters', watersRouter)
