cab6a1bbafd7e433545596fa70592db8fbefab20
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-11-21 21:42:19 +0600

    v.8.0. postgresgl_migration_vercel.json

--- server/prisma/migrations/20231120150534_one/migration.sql
+++ server/prisma/migrations/20231120150534_one/migration.sql
@@ -1,14 +1,16 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "name" TEXT NOT NULL
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Water" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "brand" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "details" TEXT NOT NULL,
@@ -16,14 +18,16 @@ CREATE TABLE "Water" (
     "imageUrl" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "userName" TEXT NOT NULL,
-    CONSTRAINT "Water_userId_userName_fkey" FOREIGN KEY ("userId", "userName") REFERENCES "User" ("id", "name") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "Water_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Token" (
-    "userId" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
     "refreshToken" TEXT NOT NULL,
-    CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("userId")
 );
 
 -- CreateIndex
@@ -37,3 +41,9 @@ CREATE UNIQUE INDEX "User_id_name_key" ON "User"("id", "name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Token_refreshToken_key" ON "Token"("refreshToken");
+
+-- AddForeignKey
+ALTER TABLE "Water" ADD CONSTRAINT "Water_userId_userName_fkey" FOREIGN KEY ("userId", "userName") REFERENCES "User"("id", "name") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
