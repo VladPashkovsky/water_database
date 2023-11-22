fa1f130e80314d8193eddfe22e881ae2ca6484ff
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-11-22 21:41:22 +0600

    v.8.9. cors_update

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 // app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }))
-app.use(cors(allowCors()))
+app.use(cors(allowCors))
 
 app.use('/api/users', usersRouter)
 app.use('/api/waters', watersRouter)
