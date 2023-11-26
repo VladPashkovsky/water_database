9620f108483c58925d630dce186c361bab3ce0a7
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-11-26 17:14:13 +0100

    v.9.4. final

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
 const errorMiddleware = require('./middlewares/error-middleware')
-const allowCors = require('./middlewares/allowCors-middleware')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 8000
@@ -23,7 +22,6 @@ app.use('/api/users', usersRouter)
 app.use('/api/waters', watersRouter)
 
 app.use(errorMiddleware)
-app.use(allowCors)
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
