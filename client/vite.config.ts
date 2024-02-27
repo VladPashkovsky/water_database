4bfceb214ac495cb427fee942acda68cf991ceb3
VladPashkovsky <v.v.pashkovsky@gmail.com>
2024-02-27 20:01:17 +0100

    v.10.9.6. update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {host: '91.200.151.134', port: 80}
-
+  server: {open: true},
+  build: {outDir: 'build', sourcemap: true},
 })
